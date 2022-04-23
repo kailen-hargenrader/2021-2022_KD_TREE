dba9952f4f025168ad3f2769c3056be3ebdc92a8
kailen-hargenrader <khargenrader@maret.org>
2022-04-23 18:26:24 -0400

    feat : Attempt to Read Input For TestCases

--- src/test/java/PointSetTest.java
+++ src/test/java/PointSetTest.java
@@ -3,40 +3,58 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PointSetTest {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 
+public class PointSetTest {
+	PointSET pointset1;
   @Before
   public void setUp() throws Exception {
+	  pointset1 = generatePointSET("circle10.txt");
   }
 
+  public PointSET generatePointSET(String s) {
+	  In in = new In("kdtree-test-files/" + s);
+	  PointSET p = new PointSET();
+	  while(in.hasNextLine()) {
+		  p.insert(new Point2D(in.readDouble(), in.readDouble()));
+	  }
+	  return p;
+  }
   @Test
   public void testIsEmpty() {
-    fail("Not yet implemented");
+    assertFalse(pointset1.isEmpty());
   }
 
   @Test
   public void testSize() {
-    fail("Not yet implemented");
+    assertTrue(pointset1.size() == 10);
   }
 
   @Test
   public void testInsert() {
-    fail("Not yet implemented");
+    pointset1.insert(new Point2D(1,1));
   }
 
   @Test
   public void testContains() {
-    fail("Not yet implemented");
+    pointset1.contains(new Point2D(1,1));
   }
 
   @Test
   public void testRange() {
-    fail("Not yet implemented");
+    Iterable<Point2D> iterable = pointset1.range(new RectHV(-100,100,-100,100));
+    int count = 0;
+    for(Point2D p : iterable) {
+    	count++;
+    }
+    assertTrue(count == 11);
   }
 
   @Test
   public void testNearest() {
-    fail("Not yet implemented");
+    assertTrue(pointset1.nearest(new Point2D(2,2)).equals(new Point2D(1,1)));
   }
 
 }
